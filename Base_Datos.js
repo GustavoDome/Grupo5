c89580ec049c6dcbcb3cacc933dbf1c1ec4a1c6c
gustavo <148644774+GustavoDome@users.noreply.github.com>
2025-05-15 22:17:49 -0300

    Exportacion Base_Datos
    Exportacion de las funciones del archivo para la utilizacion en el main

--- Base_Datos.js
+++ Base_Datos.js
@@ -1,49 +1,41 @@
 const fs = require('fs');
-
 const BD = JSON.parse(fs.readFileSync('Usuarios.json', 'utf-8'));
-console.log(data);
 
 function buscar_usuario(nombre) {
     let nombre_modificado = nombre.toLowerCase().trim();
-    let usuario = BD.Usuarios.find(Usuarios => Usuarios.nombre===nombre_modificado)
-
+    let usuario = BD.Usuarios.find(usuario => usuario.nombre.toLowerCase().trim() === nombre_modificado);
     if (usuario) {
-    return `Nombre: ${usuario.nombre}\nEmail: ${usuario.email}\nTeléfono: ${usuario.telefono}`;
+        return `Nombre: ${usuario.nombre}\nEmail: ${usuario.email}\nTeléfono: ${usuario.telefono}`;
     } else {
-    return "Usuario no encontrado.";
+        return "Usuario no encontrado.";
     }
 }
 
 function crear_usuario(nombre, email, telefono) {
     nombre = corregir_nombre(nombre);
-
     if (email_existe(email)) {
-        console.log("El email ya está registrado.");
-        return;
+        return "El usuario ya está registrado.";
     }
-
-    let nuevoUsuario = { nombre, email, telefono };
+    let nuevoUsuario = {nombre, email, telefono};
     BD.Usuarios.push(nuevoUsuario);
     fs.writeFileSync('Usuarios.json', JSON.stringify(BD, null, 2));
     return "Usuario agregado correctamente"
 }
 
-function eliminar_usuaro(nombre) {
+function eliminar_usuario(nombre) {
     let nombre_modificado = nombre.toLowerCase().trim();
-    let index = BD.Usuarios.findIndex(usuario => usuario.nombre.toLowerCase().trim() === nombreNormalizado);
-
+    let index = BD.Usuarios.findIndex(usuario => usuario.nombre.toLowerCase().trim() === nombre_modificado);
     if (index !== -1) {
         BD.Usuarios.splice(index, 1);
         fs.writeFileSync('Usuarios.json', JSON.stringify(BD, null, 2));
+        return "Usuario eliminado.";
     } else {
-        console.log("Usuario no encontrado.");
+        return "Usuario no encontrado.";
     }
 }
 
 function mostrar_usuarios() {
-    BD.Usuarios.map(usuario => {
-        console.log(`Nombre: ${usuario.nombre}\nEmail: ${usuario.email}\nTeléfono: ${usuario.telefono}`);
-    });
+    return BD.Usuarios.forEach(usuario => {console.log(`Nombre: ${usuario.nombre}\nEmail: ${usuario.email}\nTeléfono: ${usuario.telefono}\n-------------------------------------------------------------------------------------------`);});
 }
 
 function email_existe(email) {
@@ -51,5 +43,16 @@ function email_existe(email) {
 }
 
 function corregir_nombre(nombre) {
-    return nombre.trim().replace(/\s+/g, " ");
+    let nombre_modificado = nombre.toLowerCase().trim();
+    let usuario = BD.Usuarios.find(usuario => usuario.nombre.toLowerCase().trim() === nombre_modificado);
+
+    if (usuario) {
+        usuario.nombre = usuario.nombre.trim().replace(/\s+/g, " ");
+        fs.writeFileSync('Usuarios.json', JSON.stringify(BD, null, 2));
+        return "Nombre corregido";
+    } else {
+        return "Usuario no encontrado.";
+    }
 }
+
+module.exports = {buscar_usuario,crear_usuario,eliminar_usuario,mostrar_usuarios,email_existe,corregir_nombre};
