80a40111d061a6348047f18e21259b83f4d3ba01
gustavo <148644774+GustavoDome@users.noreply.github.com>
2025-05-15 21:09:09 -0300

    Import al main.js
    Se realiza un import de las funciones principales al main

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-
-// Ejercicio 5: "Gestión de usuarios con manipulación avanzada de strings" (consola)
-
-// Objetivo:
-// Crear un sistema que administre usuarios y normalice los datos ingresados, aplicando transformaciones avanzadas de string.
-
-// Listado de pasos:
-
-// Crear la estructura de datos inicial, un array de usuarios con nombre, email y telefono.
-// Mostrar un menú interactivo en consola para gestionar usuarios.
-// Implementar la función para agregar usuarios, validando emails con match y una expresión regular.
-// Implementar la función para listar usuarios, aplicando map para mostrar datos con formato personalizado.
-// Implementar la función para buscar usuarios, usando filter y normalizando con toLowerCase.
-// Implementar la función para eliminar usuarios, usando splice.
-// Agregar una función para corregir nombres, eliminando espacios extra con trim y replace.
-// Implementar una función para verificar si un email ya existe, usando some.
-// Modularizar el código para mejorar la estructura.
-// Analizar los métodos de string aplicados en la manipulación de usuarios.
-
+let BD = {buscar_usuario,crear_usuario,eliminar_usuario,mostrar_usaurios,email_existe,corregir_nombre} = require("./Base_Datos");
 console.log(
   "Ingrese una opción:\n" +
   "1. Agregar usuario\n" +
@@ -24,33 +6,38 @@ console.log(
   "3. Buscar usuario\n" +
   "4. Eliminar usuario\n" +
   "5. Editar usuario\n" +
-  "6. Salir"
+  "6. Verificar si Email existe\n" +
+  "7. Salir"
 );
 
 async function Opcion_usuario(opcion) {
   switch(opcion) {
     case "1":
-      console.log("Agregar usuario");
+      crear_usuario();
       break;
 
     case "2":
-      console.log("Listar usuarios");
+      mostrar_usaurios();
       break;
 
     case "3":
-      console.log("Buscar usuario");
+      buscar_usuario();
       break;
 
     case "4":
-      console.log("Eliminar usuario");
+      eliminar_usuario();
       break;
 
     case "5":
-      console.log("Editar usuario");
+      corregir_nombre();
+      break;
+      
+    case "6":
+      email_existe();
       break;
 
-    case "6":
-      console.log("Salir del sistema");
+    case "7":
+      console.log("Nos vemos");
       break;
 
     default:
